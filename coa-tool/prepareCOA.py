03369301e085ac3d65ab124d617fe7ade4f21f6b
Adhil E <adhil.e@tinvio.com>
2024-06-18 16:16:49 +0530

    fix

--- coa-tool/prepareCOA.py
+++ coa-tool/prepareCOA.py
@@ -37,6 +37,7 @@ Expense - Direct Costs
 Do not add additional words. If there are 15 bank accounts given, you must return exactly 15 mapped account types. 
 """
 
+"""
 sga_chart_of_accounts = [
     "Accounts Payable", "Accounts Receivable",
     "Business Bank Account", "Cash on Hand", "Cost of Goods Sold", "Creditable Withholding Tax",
@@ -49,20 +50,25 @@ sga_chart_of_accounts = [
     "Selling, General & Administrative Expense", "Shipping Expense", "Shipping Revenue",
     "Transaction Fees & Charges", "Utility Expense", "Withholding Tax Payable"
 ]
-
-sga_prompt = """
-You are a skilled financial analyst.
-Match the given trial balance account with the most suitable account from the SG Chart of Accounts.
-Below is the list of available SG Chart of Accounts:
-""" + "\n- " + "\n- ".join(sga_chart_of_accounts) + "\n"
-
-sga_prompt += """
-1) If there is no close match, name it 'No Suitable Match'.
-2) If there are 15 bank accounts given in a batch, you must return exactly 15 mapped account types (meaning 15 values returned, even if all 15 are no suitable match), Including No Suitable Match. This is so that the format will not get messed up. 
-3) Please do not give them index numbers at all.
-4) Make sure the return list length is exactly the same as the input size length (VERY IMPORTANT PLEASE MAKE SURE FOR EVERY BATCH)
-5) Please do not have empty lines in your return. The results should all be in the next line IMPORTANT
 """
+
+
+def sga_prompt_generator(chart_of_accounts):
+    sga_prompt = """
+    You are a skilled financial analyst.
+    Match the given account names with the most suitable account from the Chart of Accounts given to you.
+    Below is the list of available Chart of Accounts:
+    """ + "\n- " + "\n- ".join(chart_of_accounts) + "\n"
+
+    sga_prompt += """
+    1) If there is no close match, name it 'No Suitable Match'.
+    2) If there are 15 bank accounts given in a batch, you must return exactly 15 mapped account types (meaning 15 values returned, even if all 15 are no suitable match), Including No Suitable Match. This is so that the format will not get messed up. 
+    3) Please do not give them index numbers at all.
+    4) Make sure the return list length is exactly the same as the input size length (VERY IMPORTANT PLEASE MAKE SURE FOR EVERY BATCH)
+    5) Please do not have empty lines in your return. The results should all be in the next line IMPORTANT
+    """
+
+    return sga_prompt
 
 
 def classify_account_types(account_names, batch_size=15):
@@ -117,12 +123,13 @@ def classify_account_types(account_names, batch_size=15):
     return results
 
 
-def recommend_sga_match(account_names, batch_size=15):
+def recommend_sga_match(coa_names,account_names, batch_size=15):
     headers = {
         'Authorization': f'Bearer {API_KEY}',
         'Content-Type': 'application/json',
     }
     results = [None] * len(account_names)
+    sga_prompt=sga_prompt_generator(coa_names)
 
     def process_batch(start_index):
         end_index = min(start_index + batch_size, len(account_names))
@@ -165,17 +172,18 @@ def recommend_sga_match(account_names, batch_size=15):
 
 def process_trial_balance(file):
     external_coa_data = pd.read_csv(file)
-    st.write(external_coa_data,"COA")
-    st.write(external_coa_data.columns,"COLS")
+    st.write(external_coa_data, "COA")
+    st.write(external_coa_data.columns, "COLS")
     account_names = external_coa_data['*Name'].tolist()
-    st.write(account_names,"ACNAMES")
+    st.write(account_names, "ACNAMES")
 
     #account_types = classify_account_types(account_names)
     #trial_balance_cleaned['Account Type'] = account_types
     #combined_accounts = [f"{name} - {type}" for name, type in zip(account_names, account_types)]  ############
     combined_accounts = [f"{name} " for name in account_names]
 
-    sga_matches = recommend_sga_match(combined_accounts)  ###############
+    coa_names=["A"]
+    sga_matches = recommend_sga_match(coa_names,combined_accounts)  ###############
     external_coa_data['SGA Match Recommendation'] = sga_matches
 
     external_coa_data['Status'] = 'Active'
@@ -195,7 +203,7 @@ st.title('Jaz COA Import Mapping Tool (SG-EN)')
 external_file = st.file_uploader("Choose the external coa file", type=["csv"])
 jaz_coa_file = st.file_uploader("Choose the jaz coa import file", type=["xlsx"])
 if external_file is not None and jaz_coa_file is not None:
-    st.write("print_sga prompt",sga_prompt)
+    st.write("print_sga prompt", sga_prompt)
     processed_data = process_trial_balance(external_file)
     st.write("Processed Data", processed_data)
     csv = convert_df_to_csv(processed_data)
