a9b9f0e971bc7ddd0e7e85edc0c6263ff816a706
Adhil E <adhil.e@tinvio.com>
2024-06-19 20:01:18 +0530

    clean logs

--- coa-tool/main.py
+++ coa-tool/main.py
@@ -107,10 +107,7 @@ def recommend_sga_match(jaz_account_details, ext_coa_account_names, ext_coa_acco
 
 
 def match_coa_using_gpt(external_coa_df, jaz_coa_df, jaz_coa_map, mapped_coa_names):
-    st.write(external_coa_df, "COA")
-    st.write(external_coa_df.columns, "COLS")
     unmapped_external_coa = external_coa_df[~(external_coa_df['*Name'].isin(mapped_coa_names))]
-    st.write("jaz template", jaz_coa_df)
     jaz_account_names = []
     jaz_account_types = []
     st.write("jaz_ans", jaz_coa_df['Name*'].tolist())
@@ -122,22 +119,17 @@ def match_coa_using_gpt(external_coa_df, jaz_coa_df, jaz_coa_map, mapped_coa_nam
         else:
             jaz_account_names.append(account_name)
             jaz_account_types.append(account_type)
-    st.write("jax an ", jaz_account_names)
     ext_coa_account_names = unmapped_external_coa['*Name'].tolist()
     ext_coa_account_types = unmapped_external_coa['*Type'].tolist()
     jaz_account_details = [f"{account_name} - {account_type}" for account_name, account_type in
                            zip(jaz_account_names, jaz_account_types)]
-    st.write("JAN", jaz_account_details)
     sga_matches = recommend_sga_match(jaz_account_details, ext_coa_account_names, ext_coa_account_types, 15)
-    st.write("SGA_matches", sga_matches)
-    st.write("len sga matches", len(sga_matches), len(ext_coa_account_names))
     for i in range(len(sga_matches)):
         if sga_matches[i] != 'No Suitable Match':
             jaz_coa_name = sga_matches[i]
             ext_coa_name = ext_coa_account_names[i]
             mapped_coa_names.add(ext_coa_name)
             filtered_df = external_coa_df[external_coa_df['*Name'] == ext_coa_name]
-            st.write("filtered df length", len(filtered_df), jaz_coa_name, ext_coa_name, filtered_df)
             if len(filtered_df) > 0:
                 filtered_row = filtered_df.iloc[0]
                 jaz_coa_map[jaz_coa_name]['Code'] = filtered_row['*Code']
@@ -192,7 +184,6 @@ def run_process():
         external_coa_data = pd.read_csv(external_coa_file)
         jaz_coa_data = pd.read_excel(jaz_coa_file, sheet_name=1)
         jaz_coa_df_columns = jaz_coa_data.columns
-        st.write("jACOADF", jaz_coa_df_columns)
         currency_flag = False
         if 'Currency*' in jaz_coa_df_columns:
             currency_flag = True
@@ -239,7 +230,6 @@ def run_process():
                     jaz_coa_map[row['jaz_sga_name']]['Status'] = 'ACTIVE'
                     jaz_coa_map[row['jaz_sga_name']]['Match Type'] = 'SGA NAME'
                     mapped_external_coa_names.add(row['*Name'])
-        st.write("GOAT", jaz_coa_map)
         jaz_coa_map, mapped_external_coa_names = match_coa_using_gpt(external_coa_data, jaz_coa_data, jaz_coa_map,
                                                                      mapped_external_coa_names)
         for p in range(len(external_coa_data)):
@@ -266,10 +256,8 @@ def run_process():
 
         for key, value in jaz_coa_map.items():
             if key in ACTIVE_ONLY_ACCOUNTS and jaz_coa_map[key]['Status'] == 'INACTIVE':
-                st.write("inactive active", key)
                 jaz_coa_map[key]['Status'] = 'ACTIVE'
 
-        st.write("Processed Data final", jaz_coa_map)
         final_df = pd.DataFrame.from_dict(jaz_coa_map, orient='index')
         # Reset the index to move the outer dictionary keys to a column
         final_df.reset_index(drop=True, inplace=True)
