976b252018df8dcf736ee10d68be26ee569341bd
Adhil E <adhil.e@tinvio.com>
2024-06-24 19:43:30 +0530

    fix

--- coa-tool-benchmarking-dummy/coa-dummy.py
+++ coa-tool-benchmarking-dummy/coa-dummy.py
@@ -15,6 +15,7 @@ ACTIVE_ONLY_ACCOUNTS = ['FX Realized Currency (Gains)/Loss',
                         'FX Rounding (Gains)/Loss',
                         'Accounts Receivable',
                         'Retained Earnings',
+                        'Input VAT Receivable'
                         'Output VAT Payable']
 
 COLUMNS_WITH_CURRENCY = ['Account Type*',
