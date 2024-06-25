677fd6ef6e3f3ab25d26836382334b90b3a0a37b
Adhil E <adhil.e@tinvio.com>
2024-06-25 19:04:15 +0530

    fix

--- coa-tool/main.py
+++ coa-tool/main.py
@@ -293,10 +293,10 @@ def run_process():
     <div style="display: flex; justify-content: center; align-items: center; height: 100%; font-size: 18px; text-align: left;">
         <div style="width: 100%;">
             <h3 style="text-align: center;">Guidelines:</h3>
-            <p style="margin: 2px 0;font-size: 16px; white-space: nowrap;">1. The COA external_file should have the full list of accounts you want for the organization account</p>
+            <p style="margin: 2px 0;font-size: 16px; white-space: nowrap;">1. The COA external_file should have the full list of accounts you want for the organization </p>
             <p style="margin: 2px 0;font-size: 16px; white-space: nowrap;">2. The external_file must have the following columns: <strong>jaz_controlled_account
             </strong>,<strong>account name</strong>,<strong>account type</strong></p>
-            <p style="margin: 2px 0;font-size: 16px; white-space: nowrap;">3. Code and description columns are optional, but will be mapped if available</p>
+            <p style="margin: 2px 0;font-size: 16px; white-space: nowrap;">3. <strong>Code</strong> and <strong>Description</strong> columns are optional, but will be mapped if available</p>
             <p style="margin: 2px 0;font-size: 16px; white-space: nowrap;">4. Download the organization’s Jaz COA import_file and upload both files in this tool</p>
             <p style="margin: 2px 0;font-size: 16px; white-space: nowrap;">5. Accounts in the external_file will be matched by AI to those in the import_file
              If they match, duplicates will be removed</p>
