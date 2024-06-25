3dcbfe64ea1e26b4b119b1ad092e2ab3c86a98da
Adhil E <adhil.e@tinvio.com>
2024-06-25 17:43:22 +0530

    fix

--- coa-tool-benchmarking-dummy/coa-dummy.py
+++ coa-tool-benchmarking-dummy/coa-dummy.py
@@ -408,16 +408,14 @@ def run_process():
     instruction_block = """
     <div style="display: flex; justify-content: center; align-items: center; height: 100%; font-size: 18px; text-align: left;">
         <div style="width: 60%;">
-            <h3 style="text-align: center;">Instructions:</h3>
-            <ol>
-                <li><strong>The COA external_file should have the full list of accounts you want for the organization account.</strong></li>
-                <li><strong>The external_file must have the following columns: jaz_controlled_account, account name, account type.</strong></li>
-                <li>Code and description columns are optional, but will be mapped if available.</li>
-                <li>Download the organization’s Jaz COA import_file and upload both files in this tool.</li>
-                <li>Accounts in the external_file will be matched by AI to those in the import_file. If they match, duplicates will be removed.</li>
-                <li>Accounts in the external_file that are not matched will be created as new account rows in the import template.</li>
-                <li>Accounts in the import_file that do not have a match on external_file will be set as deleted in the import template.</li>
-            </ol>
+            <h3 style="text-align: center;">Guidelines:</h3>
+            <p>1. The COA external_file should have the full list of accounts you want for the organization account.</p>
+            <p>2. The external_file must have the following columns: **jaz_controlled_account**, **account name**,**account type**.</p>
+            <p>3. Code and description columns are optional, but will be mapped if available.</p>
+            <p>4. Download the organization’s Jaz COA import_file and upload both files in this tool.</p>
+            <p>5. Accounts in the external_file will be matched by AI to those in the import_file. If they match, duplicates will be removed.</p>
+            <p>6. Accounts in the external_file that are not matched will be created as new account rows in the import template.</p>
+            <p>7. Accounts in the import_file that do not have a match on external_file will be set as deleted in the import template.</p>
             <p>If you have any questions or issues, contact <a href="mailto:coa-help@jaz.ai">coa-help@jaz.ai</a>.</p>
         </div>
     </div>
