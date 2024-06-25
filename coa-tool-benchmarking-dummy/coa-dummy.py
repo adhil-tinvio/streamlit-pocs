a47a945ef385406bc6f1a2fefd86cb6c355a29cc
Adhil E <adhil.e@tinvio.com>
2024-06-25 19:33:04 +0530

    fix

--- coa-tool-benchmarking-dummy/coa-dummy.py
+++ coa-tool-benchmarking-dummy/coa-dummy.py
@@ -387,7 +387,7 @@ def run_process():
     <div style="display: flex; justify-content: center; align-items: center; height: 100%; font-size: 18px; text-align: left;">
         <div style="width: 100%;">
             <h3 style="text-align: center;">Guidelines:</h3>
-            <h5>Steps:</h5>
+            <h4>Steps:</h4>
             <p style="margin: 2px 0;font-size: 16px; white-space: nowrap;">1. The COA external_file should have the full list of accounts you want for the organization </p>
             <p style="margin: 2px 0;font-size: 16px; white-space: nowrap;">2. The external_file must have the following columns: <strong>jaz_controlled_account
             </strong>,<strong>account name</strong>,<strong>account type</strong></p>
