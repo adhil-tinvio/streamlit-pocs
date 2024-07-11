6933194acfc59309f46a866ab08fb8e81ac60d6c
Adhil E <adhil.e@tinvio.com>
2024-07-11 23:39:53 +0530

    fix

--- coa-tool/main.py
+++ coa-tool/main.py
@@ -290,8 +290,7 @@ def run_process():
     guideline_block = """
             <h5 style="margin-bottom: 0; margin-top: 0;">Steps:</h5>            
             <p style="margin: 2px 0;font-size: 16px; white-space: nowrap;">1. The COA external_file should have the full list of accounts you want for the organization </p>
-            <p style="margin: 2px 0;font-size: 16px; white-space: nowrap;">2. The external_file must have the following columns: <strong>jaz_controlled_account
-            </strong>,<strong>account name</strong>,<strong>account type</strong></p>
+            <p style="margin: 2px 0;font-size: 16px; white-space: nowrap;">2. The external_file must have the following columns: <strong>jaz_controlled_account</strong>,<strong>account name</strong>,<strong>account type</strong></p>
             <p style="margin: 2px 0;font-size: 16px; white-space: nowrap;">3. <strong>Code</strong> and <strong>Description</strong> columns are optional, but will be mapped if available</p>
             <p style="margin: 2px 0;font-size: 16px; white-space: nowrap;">4. Download the organization’s Jaz COA import_file and upload both files in this tool</p>
             <p style="margin: 0 0 10px 0;font-size: 16px; white-space: nowrap;">5. Download the newly generated file and copy it over into the Jaz COA import_file worksheet for upload into Jaz!</p>
